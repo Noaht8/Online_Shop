6c034866e148fe363925a4e1a7d3675b66d0122f
Noaht8 <noahtsegay8@gmail.com>
2023-10-04 18:44:10 +0300

    Update Files

---
+++ util/validation.js
@@ -0,0 +1,30 @@
+function isEmpty(value) {
+    return !value || value.trim() === '';
+}
+
+function userCredentialsAreValid(email, password) {
+    return (
+    email && 
+    email.includes('@') && 
+    password && 
+    password.trim().length > 5
+    );
+}
+
+function userDetailsAreValid(email, password, name, street, postal, city) {
+    return (
+        userCredentialsAreValid(email, password) &&
+        !isEmpty(name) && 
+        !isEmpty(street) && 
+        !isEmpty(postal) && 
+        !isEmpty(city)
+    );
+}
+
+function passwordIsConfirmed(password, confirmPassword) {
+    return email === confirmPassword;
+}
+module.exports = {
+    userDetailsAreValid: userDetailsAreValid,
+    passwordIsConfirmed: passwordIsConfirmed
+};
