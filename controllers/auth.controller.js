5478a21b4f7804095260cc196e90c1104f766812
Noaht8 <noahtsegay8@gmail.com>
2023-10-04 17:47:15 +0300

    Update More Files

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,12 +1,73 @@
+const User = require('../models/user.model');
+const authUtil = require('../util/authentication');
+
 function getSignUp(req, res) {
     res.render('customer/auth/signup');
 }
 
-function getLogin(req, res) {
+async function signup(req, res, next) {
+    const user = new User(
+        req.body.email, 
+        req.body.password, 
+        req.body.fullname, 
+        req.body.street, 
+        req.body.postal, 
+        req.body.city
+        );
+    
+        
+    try {
+        await user.signup();
+    } catch (error) {
+        next(error);
+        return;
+    }
+    
+    
+    res.redirect('/login');
+}
 
+function getLogin(req, res, next ) {
+    res.render('customer/auth/login');
+}
+
+async function login(req, res, next) {
+    const user = new User(req.body.email, req.body.password);
+    let existingUser
+    try {
+        existingUser = await user.getUserWithSameEmail();        
+    } catch (error) {
+        next(error);
+        return;
+    }
+
+    if (!existingUser) {
+        res.redirect('/login');
+        return;
+    }
+
+    const passwordIsCorrect = await user.hasMatchingPassword(existingUser.password);
+    
+    if (!passwordIsCorrect) {
+        res.redirect('/login');
+        return;
+    }
+
+    authUtil.createUserSession(req, existingUser, function() {
+        res.redirect('/');
+    });
+
+}
+
+function logout(req, res) {
+    authUtil.destroyUserAuthSession(req);
+    res.redirect('/login');
 }
 
 module.exports = {
     getSignUp: getSignUp,
-    getLogin: getLogin
+    getLogin: getLogin,
+    signup: signup,
+    login: login,
+    logout: logout
 };
